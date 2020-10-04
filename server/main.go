3c597d484988c5bc7f85515f07c14974558693cb
Luis Arce <luis.arce22@gmail.com>
2020-10-04 23:15:38 +0000

    server a list of json users

--- server/main.go
+++ server/main.go
@@ -3,13 +3,24 @@ package main
 import (
 	"net/http"
 	"github.com/rs/cors"
+	"encoding/json"
 )
+
+type User struct {
+	Name string `json: "name"`
+	Points int `json: "points"`
+}
 
 func main() {
 	mux := http.NewServeMux()
+
+	var Users []User = []User {
+		User{Name: "Luis Arce", Points: 3000},
+		User{Name: "Daniela", Points: 2500},
+	}
+
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte("{\"hello\": \"workd\"}"))
+		json.NewEncoder(w).Encode(Users)
 	})
 
 	handler := cors.Default().Handler(mux)
