22f76fc81a7f419472f68aafde00004bdb6836d3
Luis Arce <luis.arce22@gmail.com>
2021-12-28 14:52:43 -0400

    Navigation: set current background

--- ui/src/components/layout/navigation.js
+++ ui/src/components/layout/navigation.js
@@ -1,25 +1,30 @@
 import React from 'react'
 
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
+
 
 import './styles.css'
 
 const Navigation = () => {
+
+  const location = useLocation();
+  console.log(location.pathname)
+
   return (
     <ul className={"navigation"}>
-      <li style={{textDecoration: 'none'}}>
+      <li className={location.pathname === '/' ? 'current': ''}>
         <Link to="/">Home</Link>
       </li>
-      <li>
+      <li  className={location.pathname === '/users' ? 'current': ''}>
         <Link to="/users">Users</Link>
       </li>
-      <li>
+      <li className={location.pathname === '/permissions' ? 'current': ''}>
         <Link to="/permissions">Permissions</Link>
       </li>
-      <li>
+      <li className={location.pathname === '/roles' ? 'current': ''}>
         <Link to="/roles">Roles</Link>
       </li>
-      <li>
+      <li className={location.pathname === '/about' ? 'current': ''}>
         <Link to="/about">About</Link>
       </li>
     </ul>
