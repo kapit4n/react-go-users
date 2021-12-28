c09eb550da62f26459ad3bcf0b1303a03ad8a9ee
Luis Arce <luis.arce22@gmail.com>
2021-12-28 14:01:28 -0400

    react-table: on users

--- ui/src/components/users/index.js
+++ ui/src/components/users/index.js
@@ -1,23 +1,66 @@
 import React from 'react'
+import { useTable } from 'react-table'
 
-export default function Index({ users }) {
+import './index.css'
+
+export default function Index({ columns, data }) {
+
+  console.log(data)
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data,
+  })
+
   return (
     <div>
-      Users List
-      <button>add</button>
-      <table>
-        <tr>
-          <td>Full Name</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Luis Arce</td>
-          <td>
-            <button>edit</button>
-            <button>delete</button>
-            <button>inactive</button>
-          </td>
-        </tr>
+      <table {...getTableProps()}>
+        <thead>
+          <tr>
+            <th colSpan={2}>
+              <div className="main-header">
+                <div style={{ textTransform: 'none', padding: '1rem' }}>
+                  <span className={"header-chip"}>Users :</span>
+                  <span className={"header-chip"}>13.22r Total</span>
+                  <span className={"header-chip"}>Sort By: Date Created</span>
+                </div>
+                <div>
+                  <button className={"white-button"}>List</button>
+                  <button className={"white-button"}>Filter --</button>
+                  <button className={"primary-button"}>Add User</button>
+                  <button className={"primary-button"}>...</button>
+                </div>
+              </div>
+            </th>
+          </tr>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th
+                  {...column.getHeaderProps()}>{column.render('Header')}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
       </table>
     </div>
   )
--- ui/src/pages/users/index.js
+++ ui/src/pages/users/index.js
@@ -2,7 +2,24 @@ import React from 'react'
 import Users from '../../components/users/index'
 
 export default function Index() {
+
+  const columns = React.useMemo(
+    () => [
+          {
+            Header: "Full Name",
+            accessor: 'fullName'
+          },
+          
+          {
+            Header: "Role",
+            accessor: 'role'
+          }
+    ]
+  )
+
+  const data = React.useMemo(() => ([{ fullName: 'Luis Arce', role: 'Admin' }, { fullName: 'Daniel Ruiz', role: 'Software developer' }]))
+  
   return (
-    <Users />
+    <Users columns={columns} data={data} />
   )
 }
