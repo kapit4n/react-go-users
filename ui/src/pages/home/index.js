be17d4d17feef0a4240248d0f9093f79ce6ac10d
Luis Arce <luis.arce22@gmail.com>
2021-12-29 16:24:40 -0400

    UI: Have a common http client

--- ui/src/pages/home/index.js
+++ ui/src/pages/home/index.js
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from "react"
-import axios from 'axios'
+import apiClient from "../../common/http-common"
 
 import Home from '../../components/home'
 
 export default function() {
-
   const [summaryCount, setSummaryCount] = useState({});
 
   useEffect(async () => {
-    const summaryCountResp =  await axios.get(`${process.env.REACT_APP_API_URL}/summary/count`)
+    const summaryCountResp =  await apiClient.get(`/summary/count`)
     setSummaryCount(summaryCountResp.data)
-  })
+  }, [])
 
   return (
     <Home usersCount={summaryCount.users} rolesCount={summaryCount.roles} permissionsCount={summaryCount.permissions} />
