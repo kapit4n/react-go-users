0fff4ea71e45848baf684ec4f819bcaf0e6ea655
Luis Arce <luis.arce22@gmail.com>
2022-01-18 12:19:32 -0400

    #36 Refetch data after delete a role

--- ui/src/pages/roles/index.js
+++ ui/src/pages/roles/index.js
@@ -1,7 +1,14 @@
-import React, { useState, useEffect } from 'react'
+import React, { useCallback } from 'react'
 import axios from 'axios';
+import { useQuery } from 'react-query'
 
 import Roles from '../../components/roles/index'
+
+
+const fetchDetails = async () => {
+  const roles = await axios.get(`${process.env.REACT_APP_API_URL}/roles`)
+  return roles.data.data
+}
 
 export default function Index() {
 
@@ -17,15 +24,22 @@ export default function Index() {
       }
     ]
   )
-  
-  const [data, setData] = useState([])
-  
-  useEffect(async () => {
-    const roles = await axios.get(`${process.env.REACT_APP_API_URL}/roles`)
-    setData(roles.data.data)
-  }, [])
 
-  return (
-    <Roles columns={columns} data={data} />
+  const { data, status, refetch } = useQuery("roles", fetchDetails)
+
+  const onDelete = useCallback(async (id) => {
+    await axios.delete(`${process.env.REACT_APP_API_URL}/roles/${id}`)
+    refetch()
+  })
+
+  return (<>
+    { status === 'error' && "Loading Data"}
+    { status === 'loading' && "Fetching Data"}
+    {
+      status === 'success' && (
+        <Roles columns={columns} data={data} onDelete={onDelete} />
+      )
+    }
+  </>
   )
 }
