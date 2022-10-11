e7b5814655fa5a99eafe9e11716bfb50d96771c4
Luis Arce <luis.arce22@gmail.com>
2022-10-11 17:32:38 -0400

    Update go modules

--- server/controllers/Auth.go
+++ server/controllers/Auth.go
@@ -1,10 +1,13 @@
 package controllers
 
 import (
-	"fmt"
+	"errors"
 	"net/http"
 	"server/models"
+	"strings"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
@@ -24,32 +27,6 @@ type LoginRequest struct {
 	Password string `json:"password" binding:"required"`
 }
 
-func Login(c *gin.Context) {
-	session := sessions.Default(c)
-	var user models.User
-
-	var loginRequest LoginRequest
-	c.BindJSON(&loginRequest)
-
-	fmt.Println(loginRequest)
-
-	if err := models.DB.Where("email = ?", loginRequest.Email).First(&user).Error; err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authentication failed"})
-		return
-	}
-
-	if passwordValid := CheckPasswordHash(loginRequest.Password, user.Password); !passwordValid {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authentication failed"})
-		return
-	}
-
-	user.Password = ""
-
-	session.Set(sessionkey, user)
-	session.Save()
-	c.JSON(http.StatusOK, gin.H{"message": "Login successful"})
-}
-
 func Logout(c *gin.Context) {
 	session := sessions.Default(c)
 	user := session.Get(sessionkey)
@@ -65,3 +42,107 @@ func Logout(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, gin.H{"message": "Successfully logout out"})
 }
+
+type UnsignedResponse struct {
+	Message interface{} `json:"message"`
+}
+
+type SignedResponse struct {
+	Token   interface{} `json:"token"`
+	Message interface{} `json:"message"`
+}
+
+func login(c *gin.Context) {
+	var user models.User
+
+	loginRequest := LoginRequest{}
+	c.ShouldBindJSON(&loginRequest)
+
+	if err := models.DB.Where("email = ?", loginRequest.Email).First(&user).Error; err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authentication failed"})
+		return
+	}
+
+	if passwordValid := CheckPasswordHash(loginRequest.Password, user.Password); !passwordValid {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authentication failed"})
+		return
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims{
+		"email": loginRequest.Email,
+		"nbf":   time.Date(2022, 01, 01, 12, 0, 0, 0, time.UTC).Unix(),
+	})
+
+	tokenStr, err := token.SignedString([]byte("supersaucysecret"))
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, UnsignedResponse{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, SignedResponse{
+		Token:   tokenStr,
+		Message: "logged in",
+	})
+
+}
+
+func extractBearerToken(header string) (string, error) {
+	if header == "" {
+		return "", errors.New("bad header value given")
+	}
+
+	jwtToken := strings.Split(header, " ")
+	if len(jwtToken) != 2 {
+		return "", errors.New("incorrectly formatted authoriation header")
+	}
+
+	return jwtToken[1], nil
+}
+
+func parseToken(jwtToken string) (*jwt.Token, error) {
+	token, err := jwt.Parse(jwtToken, func(token *jwt.Token) (interface{}, error) {
+		if _, OK := token.Method.(*jwt.SigningMethodHMAC); !OK {
+			return nil, errors.New("nad signed method received")
+		}
+		return []byte("supersaucysecret"), nil
+	})
+
+	if err != nil {
+		return nil, errors.New("Bad jwt token")
+	}
+
+	return token, nil
+}
+
+func jwtTokenCheck(c *gin.Context) {
+	jwtToken, err := extractBearerToken(c.GetHeader("Authorization"))
+
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, UnsignedResponse{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	token, err := parseToken(jwtToken)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, UnsignedResponse{
+			Message: "Bard jwt token",
+		})
+		return
+	}
+	_, OK := token.Claims.(jwt.MapClaims)
+	if !OK {
+		c.AbortWithStatusJSON(http.StatusBadRequest, UnsignedResponse{
+			Message: "unable to parse claims",
+		})
+		return
+	}
+	c.Next()
+}
+
+// implementing login and check jwt
+// https://gist.github.com/mrcrilly/7703d630f9d589636d20b630245b6415
--- server/go.mod
+++ server/go.mod
@@ -4,23 +4,28 @@ go 1.15
 
 require (
 	github.com/boj/redistore v0.0.0-20180917114910-cd5dcc76aeff // indirect
-	github.com/gin-contrib/cors v1.3.1
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/contrib v0.0.0-20201101042839-6a891bf89f19
-	github.com/gin-gonic/gin v1.7.7
-	github.com/go-playground/validator/v10 v10.10.0 // indirect
+	github.com/gin-gonic/gin v1.8.1
+	github.com/go-playground/validator/v10 v10.11.1 // indirect
+	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/gorilla/sessions v1.2.1 // indirect
 	github.com/jinzhu/gorm v1.9.16
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
 	github.com/rs/cors v1.7.0 // indirect
-	github.com/ugorji/go v1.2.6 // indirect
-	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3
-	golang.org/x/sys v0.0.0-20220111092808-5a964db01320 // indirect
-	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
+	golang.org/x/crypto v0.0.0-20221010152910-d6f0a8c073c2
+	golang.org/x/net v0.0.0-20221004154528-8021a29435af // indirect
+	golang.org/x/sys v0.0.0-20221010170243-090e33056c14 // indirect
+	golang.org/x/text v0.3.8 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+	gopkg.in/go-playground/validator.v9 v9.29.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gorm.io/gorm v1.22.4 // indirect
 )
